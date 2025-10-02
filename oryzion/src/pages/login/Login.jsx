eae2d3c048f0707930f9f6cf4323c2e7cdfff9ac
Kauê Antonio Santos <kaueantoniofranciscosantos@gmail.com>
2025-10-02 15:51:26 -0300

    feat: alterando as configurações do body

--- oryzion/src/pages/login/Login.jsx
+++ oryzion/src/pages/login/Login.jsx
@@ -5,26 +5,28 @@ import Botao from '../../components/botao/Botao'
 const Login = () => {
     return (
         <>
-            <div className="paraCentralizar">
-                <div className="borda">
-                    <div className="borda_para_os_simbolos">
+            <div className="todoOLogin">
+                <div className="paraCentralizar">
+                    <div className="borda">
+                        <div className="borda_para_os_simbolos">
 
 
-                        <form action="">
-                            <div className="titulo">
-                                <h1>Login</h1>
-                            </div>
-                            <label htmlFor="">Email</label>
-                            <input type="email" />
-                            <label htmlFor="">Senha</label>
-                            <input type="password" />
-                            <div className="link">
-                                <a href="">Esqueceu a senha?</a>
-                            </div>
-                            <div className="botao">
-                                <Botao nomeBotao="Entrar" />
-                            </div>
-                        </form>
+                            <form action="">
+                                <div className="titulo">
+                                    <h1>Login</h1>
+                                </div>
+                                <label htmlFor="">Email</label>
+                                <input type="email" />
+                                <label htmlFor="">Senha</label>
+                                <input type="password" />
+                                <div className="link">
+                                    <a href="">Esqueceu a senha?</a>
+                                </div>
+                                <div className="botao">
+                                    <Botao nomeBotao="Entrar" />
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
