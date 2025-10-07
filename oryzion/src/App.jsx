446866f355ccde13bce7877b04ddce5fb3c63e8a
Agatha <agatha.g@aluno.senai.br>
2025-10-07 08:33:07 -0300

    feat: desenvolvendo a página de listagem

--- oryzion/src/App.jsx
+++ oryzion/src/App.jsx
@@ -1,25 +1,19 @@
-<<<<<<< HEAD
 import './App.css'
 import ListagemFeedback from './pages/listagemFeedback/listagemFeedback';
 import Login from './pages/login/Login';
-=======
 import ClientePerfil from "./pages/perfil_cliente/perfilCliente";
 import './App.css';
 // import Login from './pages/login/Login';
 // import Teste from "./pages/teste/Teste";
->>>>>>> a166a1031ad9d3d45c2ffffa3a261668b436b7b8
 
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
       <ListagemFeedback/>
-=======
       {/* <Teste/> */}
       <ClientePerfil/>
       {/* <Login/> */}
->>>>>>> a166a1031ad9d3d45c2ffffa3a261668b436b7b8
     </>
   );
 }
