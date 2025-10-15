912c337f3672d50813b2efb0f3497e49058fb6f1
Kauê Santos <kaueantoniofranciscosantos@gmail.com>
2025-10-15 15:52:53 -0300

    feat: TELA DE CHAMADO FINALIZADA!!!

--- oryzion/src/pages/chamado/Chamado.jsx
+++ oryzion/src/pages/chamado/Chamado.jsx
@@ -1,39 +1,56 @@
-import React from 'react'
-import './Chamado.css'
-import Botao from '../../components/botao/Botao'
+import React from 'react';
+import './Chamado.css';
+import Botao from '../../components/botao/Botao';
 import VoltarBranco from "../../components/voltarBranco/voltarBranco";
 
 const Chamado = () => {
-    return (
-        <>
-            <div className="todoOChamado">
-                <div className="paraCentralizar">
-                    <div className="borda">
-                        <div className="borda_para_os_simbolos">
-                            <div className="voltar-branco-container">
-                                <VoltarBranco />
-                            </div>
-                            <form action="">
-                                <div className="titulo_2">
-                                    <h1>Chamado</h1>
-                                </div>
-                                <label htmlFor="">Nome</label>
-                                <input className='input_chamado' type="text" placeholder='Nome completo do cliente' />
-                                <label htmlFor="">Telefone</label>
-                                <input className='input_chamado' type="tel" placeholder='(99) 9 9999-9999' />
-                                <label htmlFor="">CPF</label>
-                                <input className='input_chamado' type="text" placeholder='000.000.000-00' />
-                                <div className="espacamento_chamado"></div>
-                                <div className="botao">
-                                    <Botao nomeBotao="Cadastrar" />
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
-}
+  return (
+    <div className="todoOChamado">
+      <div className="paraCentralizar">
+        <div className="borda">
+          <div className="voltarBrancoContainer">
+            <VoltarBranco />
+          </div>
 
-export default Chamado
+          <form>
+            <div className="titulo_2">
+              <h1>Chamado</h1>
+            </div>
+
+            <label>Nome</label>
+            <input
+              className='input_chamado'
+              type="text"
+              placeholder='Nome completo do cliente'
+              required
+            />
+
+            <label>Telefone</label>
+            <input
+              className='input_chamado'
+              type="tel"
+              placeholder='(99) 9 9999-9999'
+              required
+            />
+
+            <label>CPF</label>
+            <input
+              className='input_chamado'
+              type="text"
+              placeholder='000.000.000-00'
+              required
+            />
+
+            <div className="espacamento_chamado"></div>
+
+            <div className="botao">
+              <Botao nomeBotao="Cadastrar" tipo="submit" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Chamado;
