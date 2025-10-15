e793084cfb65f606eb10caf6beb7706452c66364
Kauê Santos <kaueantoniofranciscosantos@gmail.com>
2025-10-15 17:17:24 -0300

    feat: TELA DE LISTAGEMCHAMADO FINALIZADA!!!

--- oryzion/src/pages/listagemChamado/ListagemChamado.jsx
+++ oryzion/src/pages/listagemChamado/ListagemChamado.jsx
@@ -1,77 +1,65 @@
-import React from 'react'
-import './ListagemChamado.css'
-import Header from '../../components/header/Header'
-import BarraPesquisa from '../../components/barraPesquisa/BarraPesquisa'
-import adiciona from '../../assets/img/adicionar.svg'
-import mais from '../../assets/img/MaisBotao.svg'
-import upload from '../../assets/img/Upload.svg'
-import edita from '../../assets/img/Editar.svg'
-import Footer from '../../components/footer/Footer'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './ListagemChamado.css';
+import Header from '../../components/header/Header';
+import Footer from '../../components/footer/Footer';
+import mais from '../../assets/img/MaisBotao.svg';
+import adiciona from '../../assets/img/adicionar.svg';
+import upload from '../../assets/img/Upload.svg';
+import edita from '../../assets/img/Editar.svg';
 
 const ListagemChamado = () => {
+  const chamados = [
+    { protocolo: '0001', nome: 'Cti' },
+    { protocolo: '0002', nome: 'Cti' },
+    { protocolo: '0003', nome: 'Cti' },
+    { protocolo: '0004', nome: 'Cti' },
+    { protocolo: '0005', nome: 'Cti' },
+  ];
+
   return (
     <>
       <Header />
-      <BarraPesquisa className="barra"
-        visiNega="none"
-        visiPosi="none"
-        visiNeut="none"
-        visiReso="none"
-        visiPend="none"
-      />
       <section className='layout_grid'>
-        <div className='tabela_equipe'></div>
         <div className='img_adiciona'>
-          <img src={mais} alt="" />
-          <img src={adiciona} alt="" />
+          <Link to="/chamado" className="botao_adicionar">
+            <img src={mais} alt="Mais" />
+            <img src={adiciona} alt="Adicionar Chamado" />
+          </Link>
         </div>
+
         <div className='tabela_chamados'>
-          <div className='numero_protocolo'>
+          <div className='coluna tabela_header'>
             <h3>Protocolo</h3>
-            <p>0001</p>
-            <p>0002</p>
-            <p>0003</p>
-            <p>0004</p>
-            <p>0005</p>
+            {chamados.map((c) => <p key={c.protocolo}>{c.protocolo}</p>)}
           </div>
-          <div className='chamado_nome'>
+          <div className='coluna tabela_header'>
             <h3>Nome</h3>
-            <p>Cti</p>
-            <p>Cti</p>
-            <p>Cti</p>
-            <p>Cti</p>
-            <p>Cti</p>
+            {chamados.map((c, index) => <p key={index}>{c.nome}</p>)}
           </div>
-          <div className='resumo_chamado'>
+          <div className='coluna tabela_header'>
             <h3>Resumo</h3>
-            <div><img src={mais} alt="" /></div>
-            <div><img src={mais} alt="" /></div>
-            <div><img src={mais} alt="" /></div>
-            <div><img src={mais} alt="" /></div>
-            <div><img src={mais} alt="" /></div>
+            {chamados.map((c, index) => (
+              <div key={index}><img src={mais} alt="Ver resumo" /></div>
+            ))}
           </div>
-          <div className='upload_chamado'>
+          <div className='coluna tabela_header'>
             <h3>Upload</h3>
-            <div><img src={upload} alt="" /></div>
-            <div><img src={upload} alt="" /></div>
-            <div><img src={upload} alt="" /></div>
-            <div><img src={upload} alt="" /></div>
-            <div><img src={upload} alt="" /></div>
+            {chamados.map((c, index) => (
+              <div key={index}><img src={upload} alt="Upload" /></div>
+            ))}
           </div>
-          <div className='editar_chamado'>
+          <div className='coluna tabela_header'>
             <h3>Editar</h3>
-            <div><img src={edita} alt="" /></div>
-            <div><img src={edita} alt="" /></div>
-            <div><img src={edita} alt="" /></div>
-            <div><img src={edita} alt="" /></div>
-            <div><img src={edita} alt="" /></div>
+            {chamados.map((c, index) => (
+              <div key={index}><img src={edita} alt="Editar" /></div>
+            ))}
           </div>
         </div>
       </section>
-
       <Footer />
     </>
-  )
+  );
 }
 
-export default ListagemChamado
+export default ListagemChamado;
