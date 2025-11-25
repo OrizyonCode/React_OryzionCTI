2782f59a68eb572952983e46894f880445282d2b
Agatha <agatha.g@aluno.senai.br>
2025-11-25 13:10:24 -0300

    feat: fazendo a página de listagem feedback

--- oryzion/src/components/barraPesquisa/BarraPesquisa.jsx
+++ oryzion/src/components/barraPesquisa/BarraPesquisa.jsx
@@ -1,42 +1,36 @@
-
-import FundoBarraPesquisa from "../../assets/img/BarraPesquisa.png";
-import './BarraPesquisa.css';
-import Lupa from "../../assets/img/Search.svg"
-import { Link } from "react-router-dom";
 import React, { useState, useEffect } from "react";
-import Filtro from '../../assets/img/Slider.svg'
+import Lupa from "../../assets/img/lupa2.png";
+import Filtro from "../../assets/img/Slider.svg";
+import "./BarraPesquisa.css";
 
-const BarraPesquisa = (props) => {
 
+const BarraPesquisa = ({ visibilidade }) => {
   const [mostrarMenu, setMostrarMenu] = useState(false);
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
 
+
   useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 768);
-    };
+    const handleResize = () => setIsMobile(window.innerWidth <= 768);
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-  const toggleMenu = () => {
-    setMostrarMenu(!mostrarMenu);
-  };
+
+  const toggleMenu = () => setMostrarMenu(!mostrarMenu);
+
 
   return (
     <div className="barra_pesquisa_container">
-      {isMobile && props.visibilidade !== "none" && (
+      {/* Filtro Mobile */}
+      {isMobile && visibilidade !== "none" && (
         <div className="filtro_container">
-          <img
-            src={Filtro}
-            alt="Filtro"
-            className="filtro_icon"
-            onClick={toggleMenu}
-          />
+          <img src={Filtro} alt="Filtro" className="filtro_icon" onClick={toggleMenu} />
+
+
           {mostrarMenu && (
             <div className="filtro_menu">
-              <div className="filtro_item">Positivos</div>
               <div className="filtro_item">Negativos</div>
+              <div className="filtro_item">Positivos</div>
               <div className="filtro_item">Neutros</div>
               <div className="filtro_item">Resolvidos</div>
               <div className="filtro_item">Pendentes</div>
@@ -45,23 +39,27 @@ const BarraPesquisa = (props) => {
         </div>
       )}
 
-      {/* Barra de pesquisa */}
+
+      {/* Barra de Pesquisa */}
       <div className="search_box">
         <img src={Lupa} alt="Pesquisar" className="lupa_icon" />
-        <input type="text" placeholder="Pesquise aqui" />
+        <input type="text" placeholder="Pesquise por feedback..." />
       </div>
 
-      {!isMobile && props.visibilidade !== "none" && (
+
+      {/* Filtro Desktop */}
+      {!isMobile && visibilidade !== "none" && (
         <ul className="links">
-          <li>Positivos</li>
           <li>Negativos</li>
-          <li>Neutros</li>
+          <li>Positivos</li>
+          <li className="ativo">Neutros</li>
           <li>Resolvidos</li>
           <li>Pendentes</li>
         </ul>
       )}
     </div>
   );
-}
+};
+
 
 export default BarraPesquisa;
